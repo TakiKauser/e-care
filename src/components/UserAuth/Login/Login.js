652fb6c4718dcadc672c8b1b6adcd3f04bb975c3
TakiKauser <takikauserbadhon@gmail.com>
2021-10-19 19:32:17 +0600

    registered-user-login

--- src/components/UserAuth/Login/Login.js
+++ src/components/UserAuth/Login/Login.js
@@ -1,11 +1,34 @@
 import React from 'react';
+import { Button, Form } from 'react-bootstrap';
 import { useLocation } from 'react-router-dom';
 import { useHistory } from 'react-router-dom';
 import { Link } from 'react-router-dom';
+import initAuth from '../../../Firebase/firebase.init';
 import useAuth from '../../../hooks/useAuth';
 import './Login.css';
 
+initAuth();
+
 const Login = () => {
+
+    const { isLogin, setIsLogin, setEmail, setPassword, setName, handleRegistration, error } = useAuth();
+
+    const toggleLogin = e => {
+        setIsLogin(e.target.checked)
+    }
+
+    const handleNameChange = e => {
+        setName(e.target.value);
+    }
+    const handleEmailChange = e => {
+        setEmail(e.target.value);
+    }
+
+    const handlePasswordChange = e => {
+        setPassword(e.target.value)
+    }
+
+    // ----------------------------------------------------
     const { signInUsingGoogle } = useAuth();
     const location = useLocation();
     const history = useHistory();
@@ -17,7 +40,6 @@ const Login = () => {
                 history.push(redirect_url);
             })
     }
-
     return (
         <div>
             <h3>Log In</h3>
@@ -32,6 +54,49 @@ const Login = () => {
                 className="regular-btn"
                 onClick={handleGoogleLogIn}
             >Google Sign In</button>
+            <div>
+                <hr />
+                <div className='mx-auto mt-3 w-50'>
+                    <Form onSubmit={handleRegistration}>
+                        <h3 className="text-primary">Please {isLogin ? 'Login' : 'Register'}</h3>
+                        {!isLogin && <div className="mb-3 row">
+                            <label htmlFor="inputName" className="col-sm-2 col-form-label">Name</label>
+                            <div className="col-sm-10">
+                                <input type="text" onBlur={handleNameChange} className="form-control" id="inputName" placeholder="Your Name" />
+                            </div>
+                        </div>}
+                        <div className="mb-3 row">
+                            <label htmlFor="inputEmail3" className="col-sm-2 col-form-label">Email</label>
+                            <div className="col-sm-10">
+                                <input onBlur={handleEmailChange} type="email" className="form-control" id="inputEmail3" required />
+                            </div>
+                        </div>
+                        <div className="mb-3 row">
+                            <label htmlFor="inputPassword3" className="col-sm-2 col-form-label">Password</label>
+                            <div className="col-sm-10">
+                                <input type="password" onBlur={handlePasswordChange} className="form-control" id="inputPassword3" required />
+                            </div>
+                        </div>
+                        <div className="mb-3 row">
+                            <div className="col-sm-10 offset-sm-2">
+                                <div className="form-check">
+                                    <input onChange={toggleLogin} className="form-check-input" type="checkbox" id="gridCheck1" />
+                                    <label className="form-check-label" htmlFor="gridCheck1">
+                                        Already Registered?
+                                    </label>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="mb-3 row text-danger">{error}</div>
+                        <button type="submit" className="btn btn-primary">
+                            {isLogin ? 'Login' : 'Register'}
+                        </button>
+                    </Form>
+
+                    <h2>or</h2>
+                    <Button onClick={handleGoogleLogIn}>Google Sign In</Button>
+                </div>
+            </div>
         </div>
     );
 };
--- src/hooks/useFirebase.js
+++ src/hooks/useFirebase.js
@@ -1,7 +1,8 @@
 import { useState } from "react";
 import { useEffect } from "react";
-import { getAuth, signInWithPopup, GoogleAuthProvider, onAuthStateChanged, signOut, createUserWithEmailAndPassword } from "firebase/auth";
+import { getAuth, signInWithPopup, GoogleAuthProvider, onAuthStateChanged, signOut, createUserWithEmailAndPassword, signInWithEmailAndPassword, updateProfile } from "firebase/auth";
 import initAuth from "../Firebase/firebase.init";
+import { useHistory } from "react-router";
 
 initAuth();
 
@@ -9,8 +10,13 @@ const useFirebase = () => {
     const [user, setUser] = useState({});
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const [error, setError] = useState('');
+    const [isLogin, setIsLogin] = useState(false);
+    const [name, setName] = useState("");
 
     const auth = getAuth();
+
+    const history = useHistory();
 
     const googleProvider = new GoogleAuthProvider();
 
@@ -21,6 +27,60 @@ const useFirebase = () => {
     const signInUsingEmailPassword = () => {
         return createUserWithEmailAndPassword(auth, email, password);
     }
+
+
+    const handleRegistration = e => {
+        e.preventDefault();
+        console.log(email, password);
+        if (password.length < 6) {
+            setError('Password Must be at least 6 characters long.')
+            return;
+        }
+        if (!/(?=.*[A-Z].*[A-Z])/.test(password)) {
+            setError('Password Must contain 2 upper case');
+            return;
+        }
+
+        if (isLogin) {
+            processLogin(email, password);
+        }
+        else {
+            registerNewUser(email, password);
+        }
+
+    }
+
+    const processLogin = (email, password) => {
+        signInWithEmailAndPassword(auth, email, password)
+            .then(result => {
+                history.push("/")
+                const user = result.user;
+                console.log(user);
+                setError('');
+            })
+            .catch(error => {
+                setError(error.message);
+            })
+    }
+
+    const registerNewUser = (email, password) => {
+        createUserWithEmailAndPassword(auth, email, password)
+            .then(result => {
+                const user = result.user;
+                console.log(user);
+                setError('');
+                // verifyEmail();
+                setUserName();
+            })
+            .catch(error => {
+                setError(error.message);
+            })
+    }
+    const setUserName = () => {
+        updateProfile(auth.currentUser, { displayName: name })
+            .then(result => { })
+    }
+
 
     const logOut = () => {
         signOut(auth)
@@ -47,6 +107,13 @@ const useFirebase = () => {
         setEmail,
         setPassword,
         signInUsingEmailPassword,
+        error,
+        setError,
+        isLogin,
+        setIsLogin,
+        name,
+        setName,
+        handleRegistration
     }
 }
 
